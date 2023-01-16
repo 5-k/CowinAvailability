013c70b62fb677e9072fabbf345761d6db7d0bae
Prateek Mishra <pramishr@microsoft.com>
2023-01-16 12:27:32 +0530

    Updated messages

--- src/main/java/com/prateek/cowinAvailibility/service/chatbot/CowinTelegramChatBot.java
+++ src/main/java/com/prateek/cowinAvailibility/service/chatbot/CowinTelegramChatBot.java
@@ -30,15 +30,11 @@ public class CowinTelegramChatBot {
         messageText = messageText.trim().toLowerCase();
 
         List<String> responseList = new ArrayList<>();
-        responseList.add("Thanks for using the chatbot.");
-        responseList.add("Since the inception of the application enough, I was fortuntate enough to recieve thousands of customers and a wonderful feedback from you all. I am happy that this application was able to help (even though a little bit)"+
-        "thousands of my fellow Indians in scheduling their covid 19 vaccinations. We all have suffered lose or may know someone who suffered. I wanted to change and minimize the impact using this application"
-        +"I hope you and your family are safe now.");
-        responseList.add(" \n\nSince the vaccine is now available directly, I have decomissioned the application now. ");
-        responseList.add("I highly appreciate your confidence in the application and want to thnkyou again for using it.");
-        responseList.add("Sincerly \nPrateek Mishra");
+        responseList.add("Thankyou for using the Covid 19 vaccine alert generator and slot schedular telegram chatbot.");
+        responseList.add("Since the inception of the application enough, I was fortunate enough to receive thousands of customers and a wonderful feedback from you all.\n\nI am happy that this application was able to help (even though a little bit) thousands of my fellow Indians in scheduling their covid 19 vaccinations.\n\nWe all have suffered lose or may know someone who suffered. I wanted to help people get vaccines so that together we fight this virus.\n\n With the help of this application I was able to help people and received their blessings. :)");
+        responseList.add("\n\nSince the vaccine is now available directly, I have decommissioned the application now.\nI highly appreciate your confidence in the application and want to Thank you again for using the application\n\n Sincerely \nPrateek Mishra");
         responseList.add("Linkedin: https://www.linkedin.com/in/prateek-mishra-61aa4658/ ");
-        responseList.add("Read Success story here: https://medium.com/p/9426aefe1a03");
+        responseList.add("\nRead Success story & provide feedbacks here: https://medium.com/p/9426aefe1a03");
         return responseList;
     }
 
